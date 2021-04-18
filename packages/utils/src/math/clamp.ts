43abda560f5a59e047d94e5bc98b841842837bc3
Ivan Juarez <ivan@blindgazer.com>
2021-04-18 15:55:54 -0500

    chore: clamp docs

--- packages/utils/src/math/clamp.ts
+++ packages/utils/src/math/clamp.ts
@@ -1,3 +1,10 @@
+/**
+ * Limits a number into a given range
+ * @param value The value to limit/clamp
+ * @param min The minimum value
+ * @param max The maximum value
+ * @returns The clampled value
+ */
 export function clamp(value: number, min: number, max: number): number {
   return Math.max(Math.min(max, value), min);
 }
