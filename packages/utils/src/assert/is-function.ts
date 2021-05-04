4d5db1e78414546d3302b6a8f4953aa74b945a42
Ivan Juarez <ivan@blindgazer.com>
2021-05-04 14:19:09 -0500

    fix: keeping function type inference

--- packages/utils/src/assert/is-function.ts
+++ packages/utils/src/assert/is-function.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { getType } from './get-type'
 
 /**
  * Tests if any given value is of type Function
  * @param value Target value to test
  */
-export function isFunction(value: unknown): boolean {
+export function isFunction(value: unknown): value is Function {
   return getType(value) === 'function';
 }
